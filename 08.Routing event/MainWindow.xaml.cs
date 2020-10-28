00e269f795cea470dc87d2ec172012580543a880
AOYLAOTANG <41245006+AOYLAOTANG@users.noreply.github.com>
2020-10-28 18:00:34 +0800

    Add files via upload

---
+++ 08.Routing event/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _08.Routing_event
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        protected int eventcount = 0;
+
+        private void SomethingClicked(object sender, RoutedEventArgs e)
+        {
+            eventcount++;
+            string message = "#" + eventcount.ToString() + ":\r\n" +
+                "Sender:" + sender.ToString() + ":\r\n" +
+                "Source:" + e.Source + ":\r\n" +
+                "Original Source:" + e.OriginalSource;
+            ListMessages.Items.Add(message);
+            e.Handled = (bool)CheckHandle.IsChecked;
+        }
+
+        private void cmdClear_Click(object sender, RoutedEventArgs e)
+        {
+            eventcount = 0;
+            ListMessages.Items.Clear();
+        }
+    }
+}
---
+++ 10.Mouse Input/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _10.Mouse_Input
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            //初始化拖放的源元素
+            Label lbl = (Label)sender;
+            DragDrop.DoDragDrop(lbl, lbl.Content, DragDropEffects.Copy);
+        }
+
+        private void label2_Drop(object sender, DragEventArgs e)
+        {
+            //设置拖放的接收元素
+            ((Label)sender).Content = e.Data.GetData(DataFormats.Text);
+        }
+
+        //private void rect_MouseMove(object sender, MouseEventArgs e)
+        //{
+        //    Point pt = e.GetPosition(this);
+        //    posInfo.Text = "鼠标位置：" + pt.ToString();
+        //}
+
+        //private void cmdCapture_Click(object sender, RoutedEventArgs e)
+        //{
+        //    Mouse.Capture(this.rect);
+        //    this.cmdCapture.Content = "鼠标已捕获";
+        //}
+    }
+}
