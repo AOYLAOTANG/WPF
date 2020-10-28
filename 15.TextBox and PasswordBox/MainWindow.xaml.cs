9e340e4b04dac92a3662ac25c68ca1e4c0b8a450
AOYLAOTANG <41245006+AOYLAOTANG@users.noreply.github.com>
2020-10-28 18:10:14 +0800

    Add files via upload

---
+++ 15.TextBox and PasswordBox/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _15.TextBox_and_PasswordBox
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_SelectionChanged(object sender, RoutedEventArgs e)
+        {
+            if (this.txtSelection == null) return;
+            this.txtSelection.Text = "选中文本从第" + (this.textBox1.SelectionStart+1) + "个字到第" 
+                + this.textBox1.SelectionLength + "个字，选中内容为：" + this.textBox1.SelectedText;
+        }
+    }
+}
---
+++ 16.List and Combo Control/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _16.List_and_Combo_Control
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void listBox1_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            if (listBox1.SelectedItem == null) return;
+            this.textSelection.Text = "选择的选项为：" + (listBox1.SelectedIndex+1) + "\r\n"
+                + "选中状态为:" + ((CheckBox)listBox1.SelectedItem).IsChecked;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (CheckBox item in listBox1.Items)
+            {
+                if (item.IsChecked == true)
+                {
+                    sb.Append(item.Content);
+                    sb.Append("被选中\r\n");
+                }
+                textSelection.Text = sb.ToString();
+            }
+        }
+    }
+}
