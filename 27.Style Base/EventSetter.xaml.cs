674fd4518f383d45352e284ba505a783436917eb
AOYLAOTANG <41245006+AOYLAOTANG@users.noreply.github.com>
2020-10-28 18:29:23 +0800

    Add files via upload

---
+++ 27.Style Base/EventSetter.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace _27.Style_Base
+{
+    /// <summary>
+    /// EventSetter.xaml 的交互逻辑
+    /// </summary>
+    public partial class EventSetter : Window
+    {
+        public EventSetter()
+        {
+            InitializeComponent();
+        }
+        private void element_MouseEnter(object sender, MouseEventArgs e)
+        {
+            ((TextBlock)sender).Background = new SolidColorBrush(Colors.LightGreen);
+        }
+
+        private void element_MouseLeave(object sender, MouseEventArgs e)
+        {
+            ((TextBlock)sender).Background = null;
+        }
+    }
+}
